4a8de61dcbde358a178116df862898ff86233af8
Callie Porter-Borden <porterhyphenborden@gmail.com>
2019-08-27 12:45:52 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//1. What is the Big O for this?
+    //1. O(1)
+    //2. O(n)
+//2. Even or odd
+    //O(1) - This function takes one value as a parameter, and performs one operation on it.
+//3. Are you here?
+    //O(n^2) - This function has 2 levels of loops.
+//4. Doubler
+    //O(n) - This function performs an operation on each item in an array, thus the time to complete the function is directly proportional to the number of items in the array.
+//5. Naive search
+    //O(n) - This function looks at each item in an array (in the worst case), so O is dependent on the length of the array.
+//6. Creating pairs
+    //O(n^2) - This function has 2 levels of loops.
+//7. Compute the sequence
+    //Creates the fibonacci sequence up to (num) places. This has an O(n)--the time it takes to complete depends on the value of the num parameter.
+//8. An efficient search
+    //O(log(n)) - This function cuts the array in half each time it loops through.
+//9. Random element
+    //O(1) - This function returns a single element, no matter the size of the array.
+//10. What Am I?
+    //This algorithm determines if a number is prime.  It has O(n)--the larger the number, the longer it could take to determine.
